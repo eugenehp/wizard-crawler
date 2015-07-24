2f37e23781d2b46a6c2d0955448b57b0d6aa137c
Eugene HP <eugene.hp2012@gmail.com>
2015-07-24 20:42:34 +0200

    improved selector

--- template.js
+++ template.js
@@ -1,5 +1,5 @@
 // creating the template for a specific URL
-var ELEMENTS = ['data','selectRootElement','selectDates','selectTitles','selectDescriptions','selectLink','selectImage','selectStartTime','selectEndTime'];
+var ELEMENTS = ['data','selectRootElement','selectDates','selectStart','selectEnd','selectTitles','selectDescriptions','selectLink','selectImage','selectStartTime','selectEndTime'];
 
 $(document).ready(function(){
 
@@ -17,9 +17,7 @@ $(document).ready(function(){
 	});
 
 	$('#clearStorage').click(clearStorage);
-	$('#applyDates').click(doApply);
-	$('#applyTitles').click(doApply);
-	$('#applyDescriptions').click(doApply);
+	$('.apply-button').click(doApply);
 
 	$('#saveTemplate').click(saveTemplate);
 	$('#exportCSV').click(exportCSV);
@@ -50,6 +48,186 @@ function exportCSV(){
 		
 	});
 }
+
+function updateView() {
+	chrome.storage.local.get(ELEMENTS,function(data){
+
+		$('#debug').val( JSON.stringify( data, null, 2 ) );
+
+		var selectDatesSplit	 	= '';
+		var selectDatesPart 		= '';
+		var selectStartSplit 		= '';
+		var selectStartPart 		= '';
+		var selectEndSplit 			= '';
+		var selectEndPart 			= '';
+		var selectTitlesSplit 		= '';
+		var selectTitlesPart 		= '';
+		var selectDescriptionsSplit = '';
+		var selectDescriptionsPart 	= '';
+
+		if(data['selectDates']) {
+			$('#selectDatesSelector').val( data['selectDates'].selector );
+
+			selectDatesSplit = data['selectDates'].split;
+			selectDatesPart = data['selectDates'].part;
+			$('#selectDatesSplit').val(selectDatesSplit);
+			$('#selectDatesPart').val(selectDatesPart || 'first');
+		}
+		if(data['selectStart']) {
+			$('#selectStartSelector').val( data['selectStart'].selector );
+
+			selectStartSplit = data['selectStart'].split;
+			selectStartPart = data['selectStart'].part;
+			$('#selectStartSplit').val(selectStartSplit);
+			$('#selectStartPart').val(selectStartPart || 'first');
+		}
+		if(data['selectEnd']) {
+			$('#selectEndSelector').val( data['selectEnd'].selector );
+
+			selectEndSplit = data['selectEnd'].split;
+			selectEndPart = data['selectEnd'].part;
+			$('#selectEndSplit').val(selectEndSplit);
+			$('#selectEndPart').val(selectEndPart || 'first');
+		}
+		if(data['selectTitles']) {
+			$('#selectTitlesSelector').val( data['selectTitles'].selector );
+
+			selectTitlesSplit = data['selectTitles'].split;
+			selectTitlesPart = data['selectTitles'].part;
+			$('#selectTitlesSplit').val(selectTitlesSplit);
+			$('#selectTitlesPart').val(selectTitlesPart || 'first');
+		}
+		if(data['selectDescriptions']) {
+			$('#selectDescriptionsSelector').val( data['selectDescriptions'].selector );
+
+			selectDescriptionsSplit = data['selectDescriptions'].split;
+			selectDescriptionsPart = data['selectDescriptions'].part;
+			$('#selectDescriptionsSplit').val(selectDescriptionsSplit);
+			$('#selectDescriptionsPart').val(selectDescriptionsPart || 'first');
+		}
+		if(data['selectLink']) {
+			$('#selectLinkSelector').val( data['selectLink'].selector );
+		}
+		if(data['selectImage']) {
+			$('#selectImageSelector').val( data['selectImage'].selector );
+		}
+
+		var collection = data.data;
+		var length = collection.length;
+		$('#tbody').children().remove();
+		
+		for (var i = 0; i < length; i++) {
+
+			var row = collection[i];
+
+			var index 			= 1 + i;
+
+			var date 		= "" || row['selectDates'];
+			var start 		= "" || row['selectStart'];
+			var end 		= "" || row['selectEnd'];
+			var title 		= "" || row['selectTitles'];
+			var description = "" || row['selectDescriptions'];
+			var link 		= "" || row['selectLink'];
+			var image 		= "" || row['selectImage'];
+
+			if(selectDatesSplit){
+				date = date.split(selectDatesSplit);
+				var part = selectDatesPart === 'first' ? 0 : 1;
+				date = date[part];
+			}
+
+			if(selectStartSplit){
+				start = start.split(selectStartSplit);
+				var part = selectStartPart === 'first' ? 0 : 1;
+				start = start[part];
+			}
+
+			if(selectEndSplit){
+				end = end.split(selectEndSplit);
+				var part = selectEndPart === 'first' ? 0 : 1;
+				end = end[part];
+			}
+
+			if(selectTitlesSplit){
+				title = title.split(selectTitlesSplit);
+				var part = selectTitlesPart === 'first' ? 0 : 1;
+				title = title[part];
+			}
+
+			if(selectDescriptionsSplit){
+				description = description.split(selectDescriptionsSplit);
+				var part = selectDescriptionsPart === 'first' ? 0 : 1;
+				description = description[part];
+			}
+
+			var row = 	"<tr><th scope='row'>"+index+"</th><td> "
+			row 	+=	date+" </td><td> ";
+			row 	+=	start+" </td><td> ";
+			row 	+=	end+" </td><td> ";
+			row 	+=	title+" </td><td> ";
+			row 	+=	description+" </td><td>";
+			row 	+=	'<a class="btn btn-default" href="'+link+'" title="'+link+'">link</a></td><td>';
+			row 	+=	"<img src='"+image+"' alt='"+image+"' width=100 height=100/></td></tr>";
+			$('#tbody').append( row );
+		};
+	})
+}
+
+function clearStorage(){
+	chrome.storage.local.clear();
+	window.close();
+}
+
+function doSelect(selectName){
+	chrome.storage.local.set({
+		current: selectName
+	},function savedNotification(){
+		window.close();
+	});
+}
+
+function doApply(){
+	chrome.storage.local.get(ELEMENTS,function(data){
+		if( !data['selectDates'] ) 			data['selectDates'] = {};
+		if( !data['selectStart'] ) 			data['selectStart'] = {};
+		if( !data['selectEnd'] ) 			data['selectEnd'] = {};
+		if( !data['selectTitles'] ) 		data['selectTitles'] = {};
+		if( !data['selectDescriptions'] ) 	data['selectDescriptions'] = {};
+		if( !data['selectLink'] ) 			data['selectLink'] = {};
+		if( !data['selectImage'] ) 			data['selectImage'] = {};
+
+		data['selectDates'].selector 			= $('#selectDatesSelector').val();
+		data['selectStart'].selector 			= $('#selectStartSelector').val();
+		data['selectEnd'].selector 				= $('#selectEndSelector').val();
+		data['selectTitles'].selector 			= $('#selectTitlesSelector').val();
+		data['selectDescriptions'].selector 	= $('#selectDescriptionsSelector').val();
+		data['selectLink'].selector 			= $('#selectLinkSelector').val();
+		data['selectImage'].selector 			= $('#selectImageSelector').val();
+
+		data['selectDates'].split 			= $('#selectDatesSplit').val();
+		data['selectStart'].split 			= $('#selectStartSplit').val();
+		data['selectEnd'].split 			= $('#selectEndSplit').val();
+		data['selectTitles'].split 			= $('#selectTitlesSplit').val();
+		data['selectDescriptions'].split 	= $('#selectDescriptionsSplit').val();
+
+		data['selectDates'].part 			= $('#selectDatesPart').val();
+		data['selectStart'].part 			= $('#selectStartPart').val();
+		data['selectEnd'].part 				= $('#selectEndPart').val();
+		data['selectTitles'].part	 		= $('#selectTitlesPart').val();
+		data['selectDescriptions'].part 	= $('#selectDescriptionsPart').val();
+
+		chrome.storage.local.set(
+			data
+		,function savedNotification(){
+			// $('#debug').val( 'doApply\n'+JSON.stringify( data, null, 2 ) );
+			updateView();
+			// window.close();
+		});
+	});
+}
+
+
+/////////////utils/////////////////
 
 function JSONToCSVConvertor(JSONData, ReportTitle, KEYS) {
     //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
@@ -112,154 +290,4 @@ function JSONToCSVConvertor(JSONData, ReportTitle, KEYS) {
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
-}
-
-function updateView() {
-	chrome.storage.local.get(ELEMENTS,function(data){
-
-		$('#debug').val( JSON.stringify( data, null, 2 ) );
-
-		var selectDatesSplit = '';
-		var selectDatesPart = '';
-		var selectTitlesSplit = '';
-		var selectTitlesPart = '';
-		var selectDescriptionsSplit = '';
-		var selectDescriptionsPart = '';
-
-		var dates 			= [];
-		var titles 			= [];
-		var descriptions 	= [];
-		var links 			= [];
-		var images 			= [];
-
-		if(data['selectDates']) {
-			$('#selectDatesSelector').val( data['selectDates'].selector );
-			dates = data['selectDates'].texts;
-
-			selectDatesSplit = data['selectDates'].split;
-			selectDatesPart = data['selectDates'].part;
-			$('#selectDatesSplit').val(selectDatesSplit);
-			$('#selectDatesPart').val(selectDatesPart || 'first');
-		}
-		if(data['selectTitles']) {
-			$('#selectTitlesSelector').val( data['selectTitles'].selector );
-			titles = data['selectTitles'].texts;
-
-			selectTitlesSplit = data['selectTitles'].split;
-			selectTitlesPart = data['selectTitles'].part;
-			$('#selectTitlesSplit').val(selectTitlesSplit);
-			$('#selectTitlesPart').val(selectTitlesPart || 'first');
-		}
-		if(data['selectDescriptions']) {
-			$('#selectDescriptionsSelector').val( data['selectDescriptions'].selector );
-			descriptions = data['selectDescriptions'].texts;
-
-			selectDescriptionsSplit = data['selectDescriptions'].split;
-			selectDescriptionsPart = data['selectDescriptions'].part;
-			$('#selectDescriptionsSplit').val(selectDescriptionsSplit);
-			$('#selectDescriptionsPart').val(selectDescriptionsPart || 'first');
-		}
-		if(data['selectLink']) {
-			links = data['selectLink'].links;
-			$('#selectLinkSelector').val( data['selectLink'].selector );
-		}
-		if(data['selectImage']) {
-			images = data['selectImage'].images;
-			$('#selectImageSelector').val( data['selectImage'].selector );
-		}
-
-		// var length = dates.length || titles.length || descriptions.length || links.length || images.length;
-		var collection = data.data;
-		var length = collection.length;
-		$('#tbody').children().remove();
-		
-		for (var i = 0; i < length; i++) {
-
-			var row = collection[i];
-
-			var index 			= 1 + i;
-			var date 			= "";  //if( dates ) if( dates.length > i ) 			date = dates[i];
-			var title 			= ""; //if( titles ) if( titles.length > i ) 			title = titles[i];
-			var description 	= ""; //if( descriptions ) if( descriptions.length > i ) 	description = descriptions[i];
-			var link 			= ""; //if( links ) if( links.length > i ) 			link = links[i];
-			var image 			= ""; //if( images ) if( images.length > i )			image = images[i];
-
-			date = row['selectDates'];
-			title = row['selectTitles'];
-			description = row['selectDescriptions'];
-			link = row['selectLink'];
-			image = row['selectImage'];
-
-			if(selectDatesSplit){
-				date = date.split(selectDatesSplit);
-				var part = selectDatesPart === 'first' ? 0 : 1;
-				date = date[part];
-			}
-
-			if(selectTitlesSplit){
-				title = title.split(selectTitlesSplit);
-				var part = selectTitlesPart === 'first' ? 0 : 1;
-				title = title[part];
-			}
-
-			if(selectDescriptionsSplit){
-				description = description.split(selectDescriptionsSplit);
-				var part = selectDescriptionsPart === 'first' ? 0 : 1;
-				description = description[part];
-			}
-
-			var row = 	"<tr><th scope='row'>"+index+"</th><td> "
-			row 	+=	date+" </td><td> ";
-			row 	+=	title+" </td><td> ";
-			row 	+=	description+" </td><td>";
-			row 	+=	'<a class="btn btn-default" href="'+link+'" title="'+link+'">link</a></td><td>';
-			row 	+=	"<img src='"+image+"' alt='"+image+"' width=100 height=100/></td></tr>";
-			$('#tbody').append( row );
-		};
-	})
-}
-
-function clearStorage(){
-	chrome.storage.local.clear();
-	window.close();
-}
-
-function doSelect(selectName){
-	chrome.storage.local.set({
-		current: selectName
-	},function savedNotification(){
-		window.close();
-	});
-}
-
-function doApply(){
-	chrome.storage.local.get(ELEMENTS,function(data){
-		if( !data['selectDates'] ) 			data['selectDates'] = {};
-		if( !data['selectTitles'] ) 		data['selectTitles'] = {};
-		if( !data['selectDescriptions'] ) 	data['selectDescriptions'] = {};
-		if( !data['selectLink'] ) 			data['selectLink'] = {};
-		if( !data['selectImage'] ) 			data['selectImage'] = {};
-
-		data['selectDates'].selector 			= $('#selectDatesSelector').val();
-		data['selectTitles'].selector 			= $('#selectTitlesSelector').val();
-		data['selectDescriptions'].selector 	= $('#selectDescriptionsSelector').val();
-		data['selectLink'].selector 			= $('#selectLinkSelector').val();
-		data['selectImage'].selector 			= $('#selectImageSelector').val();
-
-		data['selectDates'].split 			= $('#selectDatesSplit').val();
-		data['selectTitles'].split 			= $('#selectTitlesSplit').val();
-		data['selectDescriptions'].split 	= $('#selectDescriptionsSplit').val();
-
-		data['selectDates'].part 			= $('#selectDatesPart').val();
-		data['selectTitles'].part	 		= $('#selectTitlesPart').val();
-		data['selectDescriptions'].part 	= $('#selectDescriptionsPart').val();
-
-		chrome.storage.local.set(
-			data
-		,function savedNotification(){
-			// $('#debug').val( 'doApply\n'+JSON.stringify( data, null, 2 ) );
-			updateView();
-			// window.close();
-		});
-	});
 }
